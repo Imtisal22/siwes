b5ceebd487c1fbc93bad889079333cd4554965ee
Imtisal <126048673+Imtisal22@users.noreply.github.com>
2025-04-18 19:29:39 +0100

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,16 @@
+print('hello world')
+print("hello world")
+
+name = input("Enter your name ?")
+print(name)
+
+print('hello')
+print('world')
+
+print('hello \n world')
+print('adding numbers')
+x = 534 + 23
+print('dividing numbers')
+print('Yay i did it')
+
+print("hello it's a small world")
